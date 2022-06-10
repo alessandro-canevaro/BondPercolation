d9779c62339548f6c3d972b7140c7bcda988ed5f
Alessandro212977 <s212977@dtu.dk>
2022-06-10 20:08:22 +0200

    added details to plots

--- source/main.cpp
+++ source/main.cpp
@@ -5,13 +5,21 @@
 
 using namespace std;
 
+#define RUNS 5
+#define NETWORK_SIZE 1000
+#define NETWORK_TYPE 'b'
+#define PARAM1 NETWORK_SIZE
+#define PARAM2 0.003
+
 int main(){
     GiantCompSize gcs = GiantCompSize();
-    gcs.generateNetworks(5, 1000, 'b', 1000, 0.003);
+    gcs.generateNetworks(RUNS, NETWORK_SIZE, NETWORK_TYPE, PARAM1, PARAM2);
     vector<double> result = gcs.computeAverageGiantClusterSize(50);
 
     ofstream results_file("./results/node_perc_giant_cluster.csv");
     if(results_file.is_open()){
+        results_file << RUNS << ',' << NETWORK_SIZE << ',' << NETWORK_TYPE << ',' << PARAM1 << ',' << PARAM2 << endl;
+
         for(int i=0; i<result.size()-1; i++){
             results_file << result[i] << ',';
             cout << result[i] << ", ";
@@ -24,6 +32,6 @@ int main(){
     else{
         cout << "Unable to open file.";
     }
-    
+
     cout << endl << "all done" << endl;
 }
--- visualization/nodepercplot.py
+++ visualization/nodepercplot.py
@@ -3,9 +3,17 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 with open("./results/node_perc_giant_cluster.csv") as file:
+    header = next(csv.reader(file))
     row = next(csv.reader(file))
 
-row = [float(i) for i in row]
-plt.plot(np.arange(0, 1, 1/len(row)), row)
+row = [float(i)/int(header[1]) for i in row]
+
+plt.plot(np.arange(0, 1, 1/len(row)), row, label=str(header))
 plt.xlim((-0.1, 1.1))
+plt.ylim((-0.1, 1.1))
+plt.title("Average size of the largest cluster as a function of φ")
+plt.legend(loc='upper left')
+plt.xlabel("φ")
+plt.ylabel("S(φ)")
+plt.grid(True)
 plt.show()
